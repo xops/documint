52645ecbe0e385f2093c1d465351b98da07ccd90
shanejonas <jonas.shane@gmail.com>
2021-02-11 20:58:48 -0800

    fix: add portis support + remove other unnecessary providers

--- src/wallet.ts
+++ src/wallet.ts
@@ -1,31 +1,38 @@
-import WalletConnectProvider from '@walletconnect/web3-provider'
+// import WalletConnectProvider from '@walletconnect/web3-provider'
 import { ThreeIdConnect, EthereumAuthProvider } from '3id-connect'
-import Authereum from 'authereum'
+// import Authereum from 'authereum'
 import type { DIDProvider } from 'dids'
-import Fortmatic from 'fortmatic'
+// import Fortmatic from 'fortmatic'
 import Web3Modal from 'web3modal'
+import Portis from "@portis/web3";
 
 export const threeID = new ThreeIdConnect(process.env.REACT_APP_THREEID_API)
 
 export const web3Modal = new Web3Modal({
   network: 'mainnet',
-  cacheProvider: true,
+  cacheProvider: false,
   providerOptions: {
-    walletconnect: {
-      package: WalletConnectProvider,
+    // walletconnect: {
+    //   package: WalletConnectProvider,
+    //   options: {
+    //     infuraId: 'e87f83fb85bf4aa09bdf6605ebe144b7',
+    //   },
+    // },
+    // fortmatic: {
+    //   package: Fortmatic,
+    //   options: {
+    //     key: 'pk_live_EC842EEAC7F08995',
+    //   },
+    // },
+    // authereum: {
+    //   package: Authereum,
+    //   options: {},
+    // },
+    portis: {
+      package: Portis,
       options: {
-        infuraId: 'e87f83fb85bf4aa09bdf6605ebe144b7',
-      },
-    },
-    fortmatic: {
-      package: Fortmatic,
-      options: {
-        key: 'pk_live_EC842EEAC7F08995',
-      },
-    },
-    authereum: {
-      package: Authereum,
-      options: {},
+        id: "79aa26b1-16cd-4462-9dd9-49ace9c216e3"
+      }
     },
   },
 })
@@ -33,9 +40,7 @@ export const web3Modal = new Web3Modal({
 export async function getProvider(): Promise<DIDProvider> {
   const ethProvider = await web3Modal.connect()
   const addresses = await ethProvider.enable();
-  console.log("addresses", addresses);
   const authProvider = new EthereumAuthProvider(ethProvider, addresses[0]);
-  const result = await threeID.connect(authProvider);
-  console.log("connected to 3id", result);
+  await threeID.connect(authProvider);
   return threeID.getDidProvider();
 }
